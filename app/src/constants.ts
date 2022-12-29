17f1a4dbfec5a0b7f37d490c948eb17b347bf1ea
Gustav Andersson <gustav87and@gmail.com>
2022-12-30 00:39:44 +0100

    Fix development backend connection

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -1,7 +1,7 @@
 const constants = {
   port: process.env.NODE_ENV === "development" ? 5001 : 5000,
   protocol: "https",
-  backend_url: process.env.NODE_ENV === "development" ? "https://localhost:5001" : "https://backend.gstav.se"
+  backend_url: process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://backend.gstav.se"
 };
 
 export default constants;
