9f695e7f45d3be40ab176638da088b590b7a4911
Gustav Andersson <gustav87and@gmail.com>
2022-12-30 03:03:47 +0100

    More production prep

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -1,6 +1,4 @@
 const constants = {
-  port: process.env.NODE_ENV === "development" ? 5001 : 5000,
-  protocol: "https",
   backend_url: process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://backend.gstav.se"
 };
 
