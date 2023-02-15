52fb6941c43814468a04f5de4278ec1ce66571a8
Gustav Andersson <gustav87and@gmail.com>
2023-02-15 17:29:21 +0100

    Change text3

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -8,7 +8,7 @@
     <div class="box">
       <h2>What is this place?</h2>
       <p>This is my website. It contains my Curriculum Vitae, a contact form and some personal projects.</p>
-      <p>It is a place where I can try new things related to servers, programming and networking. Feel free to look around, or maybe hack the database. Enjoy!</p>
+      <p>It's a place where I can try new things related to servers, programming and networking. Feel free to look around, or maybe hack the database. Enjoy!</p>
       <p>Click here to view my <a href="/CV.pdf" v-text="item.text" @click.prevent="download(item)"></a>.</p>
     </div>
   </main>
